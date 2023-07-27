911aaf4f480b46210d30ed41a73dde8d094b9b68
devmarrie <annemarrieakeyo42@gmail.com>
2023-07-27 13:50:48 +0300

    import list

--- double_dup.py
+++ double_dup.py
@@ -38,6 +38,8 @@
 # Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
 # It does not matter what you leave beyond the returned k (hence they are underscores).
 
+from typing import List
+
 def removeDuplicates(self, nums: List[int]) -> int:
         k = 1
         count = 1
