976483a1ff7a500d61e5070a1fed7596657a02c8
devmarrie <annemarrieakeyo42@gmail.com>
2023-07-31 14:03:26 +0300

    where k can be larger than n

--- replace.py
+++ replace.py
@@ -1,7 +1,17 @@
+# def rotate(self, nums: List[int], k: int) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         for i in range(1,k+1):
+#             nums[:] = nums[-1:] + nums[:-1]
+#         return nums
+
 def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(1,k+1):
-            nums[:] = nums[-1:] + nums[:-1]
-        return nums
+        n = len(nums)
+        k = k % n  # To handle cases where k is larger than n
+
+        # Rotate the array using slicing
+        nums[:] = nums[-k:] + nums[:-k]
