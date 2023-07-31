83d63b7d06c2467c5fa6c07a8eba1610ac1600f8
devmarrie <annemarrieakeyo42@gmail.com>
2023-07-31 13:53:29 +0300

    works but time exceeded

--- replace.py
+++ replace.py
@@ -1,9 +1,7 @@
 def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
-        """            
-        if len(nums) == 2 and k > 0:
-            nums[0],nums[1] = nums[1], nums[0]
-        else:
-            nums[:] = nums[-k:] + nums[:-k]
+        """
+        for i in range(1,k+1):
+            nums[:] = nums[-1:] + nums[:-1]
         return nums
