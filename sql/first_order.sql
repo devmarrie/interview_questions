f0d494727caccb17a61bfe38a0fffef76119644c
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-02 09:38:55 +0300

    this works but not for some instances

--- sql/first_order.sql
+++ sql/first_order.sql
@@ -12,11 +12,13 @@ imm_sch AS (
        SELECT customer_id,
               order_date,
               customer_pref_delivery_date,
-              (CASE WHEN first = customer_pref_delivery_date
+              (CASE WHEN order_date = customer_pref_delivery_date
               THEN 'immidiate'
               ELSE 'scheduled'
               END) AS status
        FROM first_orders
 )
-SELECT ROUND((SUM(CASE WHEN status = 'immidiate' THEN 1 ELSE 0 END) / COUNT(1)) * 100, 2) AS immediate_percentage
-FROM imm_sch
+SELECT ROUND((SUM(CASE WHEN i.status = 'immidiate' THEN 1 ELSE 0 END) / COUNT(1)) * 100, 2) AS immediate_percentage
+FROM imm_sch AS i
+INNER JOIN first_orders AS f
+ON i.order_date = f.first
