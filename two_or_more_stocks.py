d8fc3af4131380f0a7ea662c1de24ddd76a66ad1
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-02 10:18:42 +0300

    just comparee with one before

---
+++ two_or_more_stocks.py
@@ -0,0 +1,8 @@
+def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                prof = prices[i] - prices[i-1]
+                max_profit += prof
+        return max_profit
