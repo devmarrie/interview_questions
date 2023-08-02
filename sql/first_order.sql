0dfebf1e0b63ef7408084b792c1919aaaeaae9c5
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-02 10:15:02 +0300

    working code

--- sql/first_order.sql
+++ sql/first_order.sql
@@ -1,24 +1,25 @@
-# Write your MySQL query statement below
-WITH first_orders AS (
-    SELECT delivery_id,
-           customer_id,
-           order_date,
-           customer_pref_delivery_date,
-           MIN(order_date) AS first
-    FROM Delivery
-    GROUP BY customer_id
-),
-imm_sch AS (
-       SELECT customer_id,
+WITH full_tb AS (
+       SELECT delivery_id,
+              customer_id,
               order_date,
               customer_pref_delivery_date,
+              MIN(order_date) OVER(PARTITION BY customer_id) AS first_date,
               (CASE WHEN order_date = customer_pref_delivery_date
-              THEN 'immidiate'
-              ELSE 'scheduled'
-              END) AS status
-       FROM first_orders
+                 THEN 'immediate'
+                 ELSE 'scheduled'
+                 END) AS status
+       FROM Delivery
+),
+order_del AS (
+       SELECT customer_id,
+              delivery_id,
+              first_date,
+              status
+       FROM full_tb
+       WHERE first_date = order_date
 )
-SELECT ROUND((SUM(CASE WHEN i.status = 'immidiate' THEN 1 ELSE 0 END) / COUNT(1)) * 100, 2) AS immediate_percentage
-FROM imm_sch AS i
-INNER JOIN first_orders AS f
-ON i.order_date = f.first
+SELECT ROUND((
+       (
+              SUM(CASE WHEN status = 'immediate' THEN 1 ELSE 0 END) / COUNT(*)
+       ) * 100), 2) AS immediate_percentage
+FROM order_del 
