62d4aa1e766fbc6d1a152b2c63d4fd3da9ab5c77
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-03 17:22:08 +0300

    working one

--- jump_over.py
+++ jump_over.py
@@ -1,13 +1,15 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-
+        if nums[0] == 0 and len(nums) > 1:
+           return False
         if len(nums) == 2 and nums[0] == 0:
             return False
-
+        if len(nums) == 2 and nums[1] == 0:
+            return True
         first_index = 1
         while first_index < len(nums):
             val = nums[first_index]
-            if val == 0:
+            if val == 0:    
                 return False
             first_index += val
         return True
