1167d6e202a95edf4ff568b80272335835e8be04
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-04 23:41:32 +0300

    modified one

--- sql/sub_count.sql
+++ sql/sub_count.sql
@@ -1,11 +1,4 @@
-WITH coun AS (
-  SELECT teacher_id,
-       subject_id,
-       COUNT(*) AS c
-  FROM Teacher
-  GROUP BY subject_id, teacher_id
-)
 SELECT teacher_id,
-       COUNT(*) AS cnt
-FROM coun
+       COUNT(DISTINCT subject_id) AS cnt
+FROM Teacher
 GROUP BY teacher_id
