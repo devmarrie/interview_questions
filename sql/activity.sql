a50bf219aeee199b14cccff332ed43470ee48ed8
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-05 09:23:54 +0300

    the day inclusive you pick an interval of 29

--- sql/activity.sql
+++ sql/activity.sql
@@ -50,3 +50,9 @@
 -- | 2019-07-21 | 2            |
 -- +------------+--------------+ 
 -- Explanation: Note that we do not care about days with zero active users
+SELECT activity_date AS day,
+       COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date >= DATE_SUB('2019-07-27', INTERVAL 29 DAY)
+AND activity_date <= '2019-07-27'
+GROUP BY activity_date;
