9cb9c2d39c4dce51f78fb3ad9c5d3f53ba72e933
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-08 10:17:55 +0300

    select the first year first then the rest

--- sql/firt_prod_year.sql
+++ sql/firt_prod_year.sql
@@ -63,11 +63,17 @@
 -- | 100        | 2008       | 10       | 5000  |
 -- | 200        | 2011       | 15       | 9000  |
 -- +------------+------------+----------+-------+
+WITH first AS (
+  SELECT product_id,
+         MIN(year) AS fst
+  FROM Sales
+  GROUP BY product_id
+)
 SELECT s.product_id,
-       MIN(s.year) AS first_year,
+       f.fst AS first_year,
        s.quantity,
        s.price
 FROM Sales AS s
-LEFT JOIN Product AS p
-ON s.product_id = p.product_id
-GROUP BY p.product_name
+INNER JOIN first AS f
+ON s.product_id = f.product_id
+AND s.year = f.fst
