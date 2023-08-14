a1cbc9f1a89c54f88ce4ea6c21226c5c9da93983
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-14 22:59:10 +0300

    working one

--- python/circular_tank.py
+++ python/circular_tank.py
@@ -2,16 +2,18 @@ class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         n = len(gas)
         tank = 0
+        total_tank = 0
+        start = 0
+    
         for i in range(n):
-            indx = i % n
-            tank += gas[indx]
-            count = gas[indx] - cost[indx]
-            curr_tank = count + gas[indx+1]
-            if count < 0:
-                indx += 1
+            tank += gas[i] - cost[i]
+            total_tank += gas[i] - cost[i]
+        
+            if tank < 0:
+                start = i + 1
                 tank = 0
-                break
-            
-        if curr_tank > gas[i+1]:
-                return i
-        return -1
+    
+        if total_tank < 0:
+            return -1
+        else:
+            return start
