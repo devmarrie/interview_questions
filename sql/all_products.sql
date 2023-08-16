6a22a208c7228a299b233ed6a774305437ba29de
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-16 09:37:32 +0300

    if we count custtomer id should be equal to all pd

--- sql/all_products.sql
+++ sql/all_products.sql
@@ -61,3 +61,7 @@
 -- Explanation: 
 -- The customers who bought all the products (5 and 6) are customers with IDs 1 and 3.
 
+SELECT customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(*) = (SELECT COUNT(*) FROM Product);
