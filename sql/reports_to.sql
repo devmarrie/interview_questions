eaae1c66994d242e022ab58f25d40bf5068f96a9
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-17 13:09:13 +0300

    with a self join works best

--- sql/reports_to.sql
+++ sql/reports_to.sql
@@ -1,16 +1,11 @@
-WITH emp AS (
-  SELECT reports_to,
-       COUNT(*) AS reports_count,
-       ROUND(AVG(age)) AS average_age
-  FROM Employees
-  WHERE reports_to IS NOT NULL
-  GROUP BY reports_to
-)
+
 SELECT s.employee_id,
        s.name,
-       q.reports_count,
-       q.average_age
+       COUNT(q.reports_to) AS reports_count,
+       ROUND(AVG(q.age)) AS average_age
 FROM Employees AS s
-LEFT JOIN emp AS q
+LEFT JOIN Employees AS q
 ON s.employee_id = q.reports_to
-WHERE s.reports_to is null;
+GROUP BY s.employee_id
+HAVING COUNT(q.reports_to) > 0
+ORDER BY s.employee_id;
