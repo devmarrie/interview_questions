136887966410bde6f2b9efa900ba8cdce50a4007
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-17 11:45:14 +0300

    max left and max right their min -  current height

--- python/trap_water.py
+++ python/trap_water.py
@@ -1,11 +1,22 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
-        if n == 1:
-            t = 0
-        else:
-            t = height[0]
+        if n <= 2:
+           return 0
+
+        trapped_water = 0
+        left = [0] * n
+        right = [0] * n
+
+        left[0] = height[0]
         for i in range(1, n):
-            if height[i-1] > height[i]:
-                t +=  height[i-1] - height[i]
-        return t
+            left[i] = max(left[i - 1], height[i])
+
+        right[n - 1] = height[n - 1]
+        for i in range(n - 2, -1, -1):
+            right[i] = max(right[i + 1], height[i])
+
+        for i in range(1, n - 1):
+            trapped_water += min(left[i], right[i]) - height[i]
+
+        return trapped_water
