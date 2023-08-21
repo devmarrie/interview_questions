93e142ad3466afbfe14729df919fa766ef664907
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-22 00:24:06 +0300

    union combines two select queries

--- sql/dpt.sql
+++ sql/dpt.sql
@@ -1,5 +1,10 @@
-SELECT DISTINCT e1.employee_id,
-                COALESCE(e2.department_id, e1.department_id) AS department_id
-FROM Employee AS e1
-LEFT JOIN Employee AS e2
-ON e1.employee_id = e2.employee_id AND e2.primary_flag = 'y'
+SELECT employee_id,
+       department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+UNION
+SELECT employee_id,
+       department_id
+FROM Employee
+GROUP BY employee_id
+HAVING COUNT(employee_id) = 1;
