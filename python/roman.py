bf92e994bc69f9e44437966968db78c043c6f4ce
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-21 10:38:12 +0300

    with a for loop

--- python/roman.py
+++ python/roman.py
@@ -11,3 +11,14 @@ def romanToInt(self, s: str) -> int:
                     val += rom[s[i]]
                     i += 1
         return val
+
+# For loop just minus the cureent value
+def romanToInt(self, s: str) -> int:
+        rom = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        val = 0
+        for i in range(len(s)):
+            if i+1 < len(s) and rom[s[i]] < rom[s[i+1]]:
+                val -= rom[s[i]]
+            else:
+                val += rom[s[i]]
+        return val
