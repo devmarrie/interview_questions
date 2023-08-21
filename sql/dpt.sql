0141beaf5f9adb290bc4b0873d3a176428dbb47a
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-21 12:52:03 +0300

    also works but not for subsequent

--- sql/dpt.sql
+++ sql/dpt.sql
@@ -1,12 +1,5 @@
-WITH yes AS (
-  SELECT employee_id,
-         department_id,
-         primary_flag
-  FROM Employee
-  WHERE primary_flag = 'Y'
-)
 SELECT DISTINCT e1.employee_id,
-                COALESCE(y.department_id, e1.department_id) AS department_id
+                COALESCE(e2.department_id, e1.department_id) AS department_id
 FROM Employee AS e1
-LEFT JOIN yes AS  y
-ON e1.employee_id = y.employee_id
+LEFT JOIN Employee AS e2
+ON e1.employee_id = e2.employee_id AND e2.primary_flag = 'y'
