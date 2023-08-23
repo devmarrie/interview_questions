65ccbb0e3e6ddf6dff5d7da70d58f965d3467a47
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-23 08:13:28 +0300

    question

--- sql/logs.sql
+++ sql/logs.sql
@@ -1,4 +1,52 @@
-SELECT DISTINCT a.num AS ConsecutiveNums
-FROM Logs a
-JOIN Logs b ON a.id + 1 = b.id AND a.num = b.num
-JOIN Logs c ON a.id + 2 = c.id AND a.num = c.num;
+-- Table: Logs
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | num         | varchar |
+-- +-------------+---------+
+-- In SQL, id is the primary key for this table.
+-- id is an autoincrement column.
+
+ 
+
+-- Find all numbers that appear at least three times consecutively.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Logs table:
+-- +----+-----+
+-- | id | num |
+-- +----+-----+
+-- | 1  | 1   |
+-- | 2  | 1   |
+-- | 3  | 1   |
+-- | 4  | 2   |
+-- | 5  | 1   |
+-- | 6  | 2   |
+-- | 7  | 2   |
+-- +----+-----+
+-- Output: 
+-- +-----------------+
+-- | ConsecutiveNums |
+-- +-----------------+
+-- | 1               |
+-- +-----------------+
+-- Explanation: 1 is the only number that appears consecutively for at least three times.
+
+
+
+-- Self join solulu
+-- SELECT DISTINCT a.num AS ConsecutiveNums
+-- FROM Logs a
+-- JOIN Logs b ON a.id + 1 = b.id AND a.num = b.num
+-- JOIN Logs c ON a.id + 2 = c.id AND a.num = c.num;
+
