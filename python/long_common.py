6c834323dc6716e2f35e38b580ce5f4ef3d13a78
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-25 16:14:27 +0300

    sort first before comparing the prefix

--- python/long_common.py
+++ python/long_common.py
@@ -1,11 +1,11 @@
 def longestCommonPrefix(self, strs: List[str]) -> str:
         n = len(strs)
         lcp = ""
-        if len(strs) == 1:
-            lcp += strs[0] 
-        for i in range(n):
-            if i+1 < n and i+2 < n:
-                for char in strs[i]:
-                    if char in strs[i+1] and char in strs[i+2]:
-                        lcp += char
+        strs.sort()
+        first = strs[0]
+        last = strs[n-1]
+        for i in range(len(first)):
+            if first[i] != last[i]:
+                break
+            lcp += first[i]
         return lcp
