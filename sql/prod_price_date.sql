94c88d4dc1b501906729f973cb2c8ac8bbccd56e
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-28 09:19:28 +0300

    first rank from the smallest to the largest

--- sql/prod_price_date.sql
+++ sql/prod_price_date.sql
@@ -1,6 +1,12 @@
+WITH desc_prod AS (
+  SELECT  product_id,
+       new_price AS price,
+       RANK()OVER(PARTITION BY product_id ORDER BY change_date DESC) AS rk
+  FROM Products 
+  WHERE change_date <= ' 2019-08-16'
+) 
 SELECT DISTINCT p.product_id,
-       COALESCE(CASE WHEN MAX(pr.change_date) THEN pr.new_price END , 10) AS price
+                COALESCE(d.price, 10) AS price
 FROM Products AS p
-LEFT JOIN Products AS pr
-ON p.product_id = pr.product_id AND pr.change_date <= ' 2019-08-16'
-GROUP BY pr.product_id
+LEFT JOIN desc_prod AS d
+ON p.product_id = d.product_id AND d.rk = 1
