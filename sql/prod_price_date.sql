82e8eb745ec67f54649c1a82c0f6c4f586133777
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-28 08:58:23 +0300

    what if we max the change date

--- sql/prod_price_date.sql
+++ sql/prod_price_date.sql
@@ -1,5 +1,6 @@
-SELECT product_id, 
-       COALESCE(MAX(CASE WHEN change_date <= "2019-08-16"
-               THEN new_price END), 10) AS price
-FROM Products
-GROUP BY product_id
+SELECT DISTINCT p.product_id,
+       COALESCE(CASE WHEN MAX(pr.change_date) THEN pr.new_price END , 10) AS price
+FROM Products AS p
+LEFT JOIN Products AS pr
+ON p.product_id = pr.product_id AND pr.change_date <= ' 2019-08-16'
+GROUP BY pr.product_id
