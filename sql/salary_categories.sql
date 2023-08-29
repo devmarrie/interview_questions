cad61e2f2b84362a5f35862c0804fb1236b03d82
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-29 10:35:56 +0300

    clculate categoruy separately then union them

--- sql/salary_categories.sql
+++ sql/salary_categories.sql
@@ -50,3 +50,17 @@
 -- Average Salary: No accounts.
 -- High Salary: Accounts 3, 6, and 8.
 
+SELECT 'Low Salary' AS category,
+        COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income < 20000
+UNION
+SELECT 'Average Salary' AS category,
+        COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income  >= 20000 AND income <= 50000
+UNION
+SELECT 'High Salary' AS category,
+        COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income  > 50000
