2c7871f49a4c17c3f04e2370586c532f9249b98e
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-29 11:14:12 +0300

    step -1 will take care of the zigzag

--- python/zigzag.py
+++ python/zigzag.py
@@ -31,3 +31,21 @@
 
 # Input: s = "A", numRows = 1
 # Output: "A"
+
+def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+        
+        result = [''] * numRows
+        index, step = 0 ,1
+        
+        for char in s:
+            result[index] += char
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+            index += step
+        print(result)
+        new = ''.join(result)
+        return new
