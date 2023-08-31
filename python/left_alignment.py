bb56980fd4dc57c35656bd29ddc0f539ed1f2015
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-31 13:16:10 +0300

    let the new line contain the first word and itslen

--- python/left_alignment.py
+++ python/left_alignment.py
@@ -6,14 +6,24 @@ def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
             if tt_width + len(line) + len(word) <= maxWidth:
                 line.append(word)
                 tt_width += len(word)
-            spaces = maxWidth - tt_width
-            each = spaces // max(1,len(line) - 1)
-            extra = spaces % max(1,len(line) - 1)
-            for i in range(max(1,len(line) - 1)):
-                line[i] += " " * each
-                if extra:
-                   line[i] += " "
-                   extra -= 1
-            out.append("".join(line))
-            line, tt_width = [], 0
+            else:
+                spaces = maxWidth - tt_width
+                if len(line) == 1:
+                    out.append(line[0] + " " * spaces)
+                else:
+                    gaps = len(line) - 1
+                    extra = spaces // gaps
+                    rem = spaces % gaps
+
+                    for i in range(len(line) -1):
+                        line[i] += " " * extra
+                        if rem > 0:
+                            line[i] += " "
+                            rem -= 1
+                    out.append("".join(line))
+                line = [word]
+                tt_width = len(word)
+        #left justify the curr line
+        last_line = ' '.join(line).ljust(maxWidth)
+        out.append(last_line)
         return out
