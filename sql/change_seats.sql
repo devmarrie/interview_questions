92b1083d16f1714163fe5215448913ca5d3a2104
devmarrie <annemarrieakeyo42@gmail.com>
2023-08-31 15:25:27 +0300

    is not null ve not in subquery

--- sql/change_seats.sql
+++ sql/change_seats.sql
@@ -13,4 +13,19 @@ FROM nums n
 INNER JOIN Seat s
 ON n.new = s.id
 ORDER BY n.id ASC
-    
+
+
+-- Alernative approach
+WITH alt AS (
+SELECT id,student,
+       LAG(id)OVER(ORDER BY id) AS prev,
+       LEAD(id)OVER(ORDER BY id ) AS next
+FROM Seat
+)
+SELECT CASE WHEN ((id % 2 = 1) AND next IS NOT NULL)
+         THEN next
+        WHEN (id % 2 = 0)
+          THEN prev
+        ELSE id END AS id, student
+FROM alt
+ORDER BY id ASC
