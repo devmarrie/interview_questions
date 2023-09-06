f7d5f89bc2ee6cd6678743efad4281061a80479d
devmarrie <annemarrieakeyo42@gmail.com>
2023-09-06 08:45:53 +0300

    increment i if  it finds a match j is always incr

--- python/subsequent.py
+++ python/subsequent.py
@@ -1,10 +1,17 @@
+# def isSubsequence(self, s: str, t: str) -> bool:
+#         sub = ""
+#         fst = 0
+#         n = len(s)
+#         for char in t:
+#             if fst < n and char == s[fst]:
+#                 sub += char
+#                 fst += 1
+#         print(sub)
+#         return sub == s
 def isSubsequence(self, s: str, t: str) -> bool:
-        sub = ""
-        fst = 0
-        n = len(s)
-        for char in t:
-            if fst < n and char == s[fst]:
-                sub += char
-                fst += 1
-        print(sub)
-        return sub == s
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return i == len(s)
