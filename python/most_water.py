8960caa0331ee0e614c84e92f240063ac21b593a
devmarrie <annemarrieakeyo42@gmail.com>
2023-09-15 22:09:25 +0300

    minus indexes to find the sapces and min height

--- python/most_water.py
+++ python/most_water.py
@@ -18,3 +18,19 @@
 
 # Input: height = [1,1]
 # Output: 1
+
+from typing import List
+
+def maxArea(self, height: List[int]) -> int:
+        res = 0
+        l,r = 0, len(height) - 1
+
+        while l < r:
+            area = (r - l) * min(height[l],height[r])
+            res = max(res, area)
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
