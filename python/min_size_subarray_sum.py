a0c0bc63bfb6419aaf5f1614a8778c880b8fdbf6
devmarrie <annemarrieakeyo42@gmail.com>
2023-09-19 09:39:32 +0300

    sliding window is literally just that just greater

--- python/min_size_subarray_sum.py
+++ python/min_size_subarray_sum.py
@@ -19,3 +19,16 @@
 
 # Input: target = 11, nums = [1,1,1,1,1,1,1,1]
 # Output: 0
+from typing import List
+
+def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l, total = 0, 0
+        res = float("inf")
+
+        for i in range(len(nums)):
+            total += nums[i]
+            while total >= target:
+                res = min(i-l+1, res)
+                total -= nums[l]
+                l += 1
+        return 0 if res == float("inf") else res
