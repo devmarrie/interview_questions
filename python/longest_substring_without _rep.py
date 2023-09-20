cc91ada68a287aaf5a5f90eff9767bb8c3037fa6
devmarrie <annemarrieakeyo42@gmail.com>
2023-09-20 09:58:37 +0300

    works but for the first 3 only

--- python/longest_substring_without _rep.py
+++ python/longest_substring_without _rep.py
@@ -22,3 +22,27 @@
 # Output: 3
 # Explanation: The answer is "wke", with the length of 3.
 # Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+def lengthOfLongestSubstring(self, s: str) -> int:
+        res = 0
+        i = 0
+        if s == "":
+            return 0
+        if not s.strip():
+            return 1
+        if len(s) == 1:
+            return 1
+        sub = f"{s[0]}"        
+        for j in range(1, len(s)):
+            if s[j] != s[i] and s[j] not in sub:
+                sub += s[j]
+                if len(s) == 2 and s == sub:
+                    return 2
+                i += 1
+            else:
+                res = max(len(sub), res)
+                sub = f"{s[j]}"
+                i = j
+                j = i + 1
+        print(res)
+        return res
