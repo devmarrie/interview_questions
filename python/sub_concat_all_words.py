817cb55581977fe4fa7b0b500f7a93415d6e1864
devmarrie <annemarrieakeyo42@gmail.com>
2023-09-27 08:51:45 +0300

    runs correctly but exceeds memory limit

--- python/sub_concat_all_words.py
+++ python/sub_concat_all_words.py
@@ -34,35 +34,23 @@
 # The substring starting at 9 is "barthefoo". It is the concatenation of ["bar","the","foo"] which is a permutation of words.
 # The substring starting at 12 is "thefoobar". It is the concatenation of ["the","foo","bar"] which is a permutation of words.
 
+from itertools import permutations
+from typing import List
+
 
 def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        res = ""
-        emp = []
         l = 0
-        w_cut = len(words[0])
-        wlen = len(words) * w_cut
-        tog = []
-        w_perm = set(''.join(p) for p in permutations(words))
-
-        # Hash map for how many times the word occours
-        wcnt, seen = {}, {}
-        for c in words:
-            wcnt[c] = wcnt.get(c, 0) + 1
-
+        res = ''
+        lst = []
+        w = len(words) * len(words[0])
+        perm = set(''.join(p) for p in permutations(words))
+        print(perm)
         for r in range(len(s)):
             res += s[r]
-            if len(res) == wlen:
-                emp.append(res)
-                print(res[:w_cut])
-                for perm in w_perm:
-                    if perm in res:
-                       tog.append(l)
+            while len(res) == w:
+                if res in perm:
+                   lst.append(l)
                 res = res[1:]
                 l += 1
-        print(emp)
+        return lst
         
-
-        # for i in range(len(emp)):
-        #     if any(perm in emp[i] for perm in w_perm):
-        #         tog.append(i)
-        return tog
