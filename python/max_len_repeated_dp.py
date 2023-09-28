dfdd93262d2b8eb02672eec70378b8a1e10f4d7b
devmarrie <annemarrieakeyo42@gmail.com>
2023-09-28 09:16:10 +0300

    use dynamic programming

--- python/max_len_repeated_dp.py
+++ python/max_len_repeated_dp.py
@@ -14,3 +14,14 @@
 # Output: 5
 # Explanation: The repeated subarray with maximum length is [0,0,0,0,0].
 
+def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        dp = [[0] * (len(nums2) + 1) for _ in range(len(nums1) + 1)]
+        mlen = 0
+
+        for y in range(len(nums1) -1, -1, -1):
+            for x in range(len(nums2) -1, -1, -1):
+                if nums1[y] == nums2[x]:
+                    dp[y][x] = dp[y + 1][x + 1] + 1
+                    mlen = max(dp[y][x], mlen)
+        return mlen 
+
