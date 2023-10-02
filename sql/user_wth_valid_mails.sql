b1459463295eab7dd08392b52da3f57272310ff4
devmarrie <annemarrieakeyo42@gmail.com>
2023-10-02 16:10:25 +0300

    mysql doesnt know / used foe escaping

--- sql/user_wth_valid_mails.sql
+++ sql/user_wth_valid_mails.sql
@@ -54,4 +54,12 @@
 -- The mail of user 6 does not have the leetcode domain.
 -- The mail of user 7 starts with a period.
 
+SELECT
+    user_id,
+    name,
+    mail
+  FROM
+    Users
+  WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.\-]{0,}(@leetcode\.com)$'
+  AND  mail LIKE '%@leetcode.com'
 
