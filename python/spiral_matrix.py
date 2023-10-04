bc6aa1acf6397ae2d04aacea42a77ddd8b27262c
devmarrie <annemarrieakeyo42@gmail.com>
2023-10-04 09:45:14 +0300

    iterate logically

--- python/spiral_matrix.py
+++ python/spiral_matrix.py
@@ -11,3 +11,33 @@
 
 # Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
 # Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+
+def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        l, r = 0, len(matrix[0])
+        t, b = 0, len(matrix)
+        res = []
+
+        while l < r and t < b:
+            # move left to right
+            for i in range(l, r):
+                res.append(matrix[t][i])
+            t += 1
+
+            # move from top to bottom
+            for i in range(t, b):
+                res.append(matrix[i][r - 1])
+            r -= 1
+            if not(l < r and t < b):
+                break
+
+            # move from right to left
+            for i in range(r - 1, l -1, -1):
+                res.append(matrix[b - 1][i])
+            b -= 1
+
+            # move from bottom to top
+            for i in range(b -1, t-1, -1):
+                res.append(matrix[i][l]) 
+            l += 1
+        return res
