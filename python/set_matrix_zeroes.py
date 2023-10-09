a0c88b312bf0b18349adf7a57c20b70becf11e24
devmarrie <annemarrieakeyo42@gmail.com>
2023-10-09 10:34:30 +0300

    first approach

--- python/set_matrix_zeroes.py
+++ python/set_matrix_zeroes.py
@@ -13,3 +13,34 @@
 
 # Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
 # Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+from typing import List
+def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        cols =  len(matrix[0])
+        rows =  len(matrix)
+        firstRow = False
+
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+
+                    if i > 0:
+                        matrix[i][0] = 0
+                    else:
+                        firstRow = True
+        
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+
+        if firstRow:
+            for c in range(cols):
+                matrix[0][c] = 0
+        
+        if matrix[0][0] == 0:
+            for r in range(rows):
+                matrix[r][0] = 0
