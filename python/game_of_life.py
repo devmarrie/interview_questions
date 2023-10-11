3dfed02f9bb395f29d67a92f6d8ef5f2d24f5f0a
devmarrie <annemarrieakeyo42@gmail.com>
2023-10-11 11:13:25 +0300

    create a copy of  board and fill it then  compare

--- python/game_of_life.py
+++ python/game_of_life.py
@@ -20,3 +20,40 @@
 
 # Input: board = [[1,1],[1,0]]
 # Output: [[1,1],[1,1]]
+
+from typing import List
+
+
+def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        rows = len(board)
+        cols = len(board[0])
+        copy_board = [[0]*cols for _ in range(rows)]
+
+        for r in range(rows):
+            for c in range(cols):
+                top = board[r-1][c] if r > 0 else 0
+                dleft = board[r-1][c+1] if r > 0 and c < cols - 1 else 0
+                sleft = board[r][c+1] if c < cols - 1 else 0
+                dlbot = board[r+1][c+1] if r < rows - 1 and c < cols - 1 else 0
+                bot = board[r+1][c] if r < rows - 1 else 0
+                drbot = board[r+1][c-1] if r < rows - 1 and c > 0 else 0
+                sright = board[r][c-1] if c > 0 else 0
+                dright = board[r-1][c-1] if r > 0 and c > 0 else 0
+                tt = top + dleft + sleft + dlbot + bot + drbot + sright + dright
+                copy_board[r][c] = tt
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 1 and copy_board[r][c] < 2:
+                    board[r][c] = 0
+                elif board[r][c] == 1 and copy_board[r][c] == 2:
+                    board[r][c] = 1
+                elif board[r][c] == 1 and copy_board[r][c] == 3:
+                    board[r][c] = 1
+                elif board[r][c] == 1 and copy_board[r][c] > 3:
+                    board[r][c] = 0
+                elif board[r][c] == 0 and copy_board[r][c] == 3:
+                    board[r][c] = 1
