dfa3f89d766e4dc0d55e6a319bd59ce1c2b7dd6d
devmarrie <annemarrieakeyo42@gmail.com>
2023-10-14 08:47:27 +0300

    store dict with s as values t as keys check ==

--- python/isomophric_strings.py
+++ python/isomophric_strings.py
@@ -20,3 +20,26 @@
 
 # Input: s = "paper", t = "title"
 # Output: true
+
+def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_to_t = {}
+        t_to_s = {}
+
+        for i in range(len(s)):
+            char_s, char_t = s[i], t[i]
+            if char_s in s_to_t:
+                if s_to_t[char_s] != char_t:
+                    return False
+            else:
+                s_to_t[char_s] = char_t
+
+            if char_t in t_to_s:
+                if t_to_s[char_t] != char_s:
+                    return False
+            else:
+                t_to_s[char_t] = char_s
+        print(s_to_t)
+        print(t_to_s)
+        return True
