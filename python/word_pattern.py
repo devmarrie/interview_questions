19ed79081c306bb64749c40bb932c9c42f7e128e
devmarrie <annemarrieakeyo42@gmail.com>
2023-10-16 11:08:16 +0300

    split the sentences to remove the spaces

--- python/word_pattern.py
+++ python/word_pattern.py
@@ -19,3 +19,24 @@
 # Input: pattern = "aaaa", s = "dog cat cat dog"
 # Output: false
 
+def wordPattern(pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+        to_words = {}
+        to_pat = {}
+
+        for i in range(len(pattern)):
+            char_p , word_s = pattern[i], words[i]
+            if char_p in to_pat:
+                if to_pat[char_p] != word_s:
+                    return False
+            else:
+                to_pat[char_p] = word_s
+            
+            if word_s in to_words:
+                if to_words[word_s] != char_p:
+                    return False
+                else:
+                    to_words[word_s] = char_p
+        return len(set(pattern)) == len(set(words))
