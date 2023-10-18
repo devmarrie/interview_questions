817b8f6445787e7ed602bb4de61b53d3dfd883e3
devmarrie <annemarrieakeyo42@gmail.com>
2023-10-18 09:53:01 +0300

    multiple conditions use case when

--- sql/update_salary.sql
+++ sql/update_salary.sql
@@ -47,3 +47,8 @@
 -- (1, A) and (3, C) were changed from 'm' to 'f'.
 -- (2, B) and (4, D) were changed from 'f' to 'm'.
 
+UPDATE Salary
+SET sex = CASE
+        WHEN sex = 'm' THEN 'f'
+        WHEN sex = 'f' THEN 'm'
+END;
