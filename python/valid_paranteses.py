6bff9e1a403da3aa3bd1e6836d5a103114394af3
devmarrie <annemarrieakeyo42@gmail.com>
2023-11-03 10:09:40 +0300

    hash map to match the closing to open

--- python/valid_paranteses.py
+++ python/valid_paranteses.py
@@ -22,3 +22,17 @@
 
 # Input: s = "(]"
 # Output: false
+
+def isValid(s: str) -> bool:
+        stack = []
+        opens = {')': '(', '}': '{', ']':'['}
+
+        for c in s:
+            if c in opens: # if it is a closing bracket
+                if stack and opens[c] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c) # add the opening bracket to the stack
+        return True if not stack else False
