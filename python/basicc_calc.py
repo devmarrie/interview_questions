197bfe3d4e8093b21724d5da339e6c1b287ea381
devmarrie <annemarrieakeyo42@gmail.com>
2023-11-10 12:07:53 +0300

    digit add it to curr +/- change sign (append )pop

--- python/basicc_calc.py
+++ python/basicc_calc.py
@@ -18,3 +18,32 @@
 
 # Input: s = "(1+(4+5+2)-3)+(6+8)"
 # Output: 23
+
+
+def calculate(s: str) -> int:
+        output = 0
+        curr = 0
+        stack = []
+        sign = 1
+        
+        for d in s:
+            if d.isdigit():
+                curr = curr*10 + int(d)
+            elif d in "+-":
+                output += (curr*sign)
+                curr = 0
+                if d == "-":
+                    sign = -1
+                else:
+                    sign = 1
+            elif d == "(":
+                stack.append(output)
+                stack.append(sign)
+                output = 0
+                sign = 1
+            elif d == ")":
+                output += (curr*sign)
+                output *= stack.pop()
+                output += stack.pop()
+                curr = 0
+        return output + (curr*sign)
