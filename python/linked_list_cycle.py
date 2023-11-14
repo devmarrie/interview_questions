7b0be0d2c7d8bc7a3603979b9aa676cb74bf1ab9
devmarrie <annemarrieakeyo42@gmail.com>
2023-11-14 18:20:40 +0300

    use the floid's tortoise and hare approach

--- python/linked_list_cycle.py
+++ python/linked_list_cycle.py
@@ -23,3 +23,24 @@
 # Input: head = [1], pos = -1
 # Output: false
 # Explanation: There is no cycle in the linked list.
+
+
+
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast, slow = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            #return true if they meet
+            if slow == fast:
+                return True
+        return False
