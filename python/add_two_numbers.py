791ee5954facdcc2f6ca74e744c109ec8ef78109
devmarrie <annemarrieakeyo42@gmail.com>
2023-11-15 09:25:41 +0300

    update the curr=curr.next and the lists too

--- python/add_two_numbers.py
+++ python/add_two_numbers.py
@@ -27,37 +27,34 @@ Constraints:
 """
 from typing import Optional
 
-class ListNode:
+# Definition for singly-linked list.
+class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        res = ListNode() # acts as our head
+        curr = res
 
-class LinkedList:
-     def toInt(self, head: Optional[ListNode]) -> int:
-        num = 0
-        curr_node = head
-        while curr_node:
-            num = num * 10 + curr_node.val
-            curr_node = curr_node.next
-        return num
-     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        num_1 = self.toInt(l1)
-        num_2 = self.toInt(l2)
-        sum_int = num_1 + num_2
-        
-        if sum_int == 0:
-            return ListNode(0)
-        
-        new_head = ListNode()
-        final_node = new_head
+        carry = 0
 
-        while sum_int > 0:
-            last_dig = sum_int % 10
-            sum_int //= 10
-            final_node.next = ListNode(last_dig)
-            final_node = final_node.next
-        return new_head.next
-    
-if __name__ == '__main__':
-    new_list = LinkedList()
-    new_list.addTwoNumbers([3,4,2], [5,6,7])
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            val  = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10 
+            curr.next = ListNode(val)
+
+            # update the pointers
+            curr = curr.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return res.next
