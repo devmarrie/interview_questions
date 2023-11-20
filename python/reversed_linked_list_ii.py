1bd1ee0066b3dda707df6630760f61d992b5083d
devmarrie <annemarrieakeyo42@gmail.com>
2023-11-20 15:28:31 +0300

    first find the curr then revese the linked list

--- python/reversed_linked_list_ii.py
+++ python/reversed_linked_list_ii.py
@@ -11,3 +11,31 @@
 
 # Input: head = [5], left = 1, right = 1
 # Output: [5]
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        prev = dummy
+        curr = head
+        
+        # the node at position left
+        for i in range(left - 1):
+            prev, curr = curr, curr.next
+
+        # now that we are at curr reverse the ranges
+        lprev = None
+        for x in range (right - left + 1):
+            tmpNext = curr.next
+            curr.next = lprev # curr pointer point to null or the prev node
+            lprev, curr = curr, tmpNext
+
+        #update pointers
+        prev.next.next = curr
+        prev.next = lprev
+        return dummy.next
