184ffc7b1d207fea39780bbd60ddfb238efd509f
devmarrie <annemarrieakeyo42@gmail.com>
2023-11-22 11:37:40 +0300

    find the kth node and let the revesed ointer -> k

--- python/reverse_in_kgrp.py
+++ python/reverse_in_kgrp.py
@@ -16,3 +16,38 @@
 # Input: head = [1,2,3,4,5], k = 3
 # Output: [3,2,1,4,5]
 
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        curr = head
+        dummy = ListNode(0, head)
+        grpPrev = dummy # for easy pointing to the next group
+
+        while True:
+            kth = self.getKthNode(grpPrev, k)
+            if not kth:
+                break 
+            grpNext = kth.next #after k
+
+            # now reverse the linked list
+            prev, curr = kth.next, grpPrev.next
+            while curr != grpNext:
+                tmp = curr.next
+                curr.next = prev
+                prev, curr = curr, tmp
+        
+            tmp = grpPrev.next
+            grpPrev.next = kth
+            grpPrev = tmp
+        return dummy.next
+
+    # get the group that we are trying to reverse
+    def getKthNode(self, curr, k):
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+        return curr
