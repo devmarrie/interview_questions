e50904eac21ec19442672cc8335960f315410a3c
devmarrie <annemarrieakeyo42@gmail.com>
2023-11-23 10:00:57 +0300

    find the values in linked list reverse iter to n

--- python/remove_nth_from_end.py
+++ python/remove_nth_from_end.py
@@ -14,3 +14,28 @@
 
 # Input: head = [1,2], n = 1
 # Output: [1]
+
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        curr = head
+        prevNode = dummy
+        count = 0
+
+        while curr:
+            count += 1
+            curr = curr.next
+
+        for i in range(count, 0, -1):
+            if i == n:
+                prevNode.next = prevNode.next.next
+            prevNode = prevNode.next
+
+        return dummy.next
