0124ede398cbd9570489b1f27e54ad827c940da6
devmarrie <annemarrieakeyo42@gmail.com>
2023-11-24 08:40:42 +0300

    the trick is in curr=dummy cause of head

--- python/remove_dupl_frm_sorted_ii.py
+++ python/remove_dupl_frm_sorted_ii.py
@@ -11,3 +11,23 @@
 
 # Input: head = [1,1,1,2,3]
 # Output: [2,3]
+
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        curr = dummy
+        
+        while curr:
+            if curr.next and curr.next.next and curr.next.val == curr.next.next.val:
+                tmp = curr.next.next
+                while tmp and tmp.next and tmp.val == tmp.next.val:
+                    tmp = tmp.next
+                curr.next = tmp.next
+            else:
+                curr = curr.next
+        return dummy.next
