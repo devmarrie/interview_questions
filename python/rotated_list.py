9d548b7fbcc529f11a9f6dcde927288a30de436c
devmarrie <annemarrieakeyo42@gmail.com>
2023-11-27 12:50:25 +0300

    start from the element before  the one to rotate

--- python/rotated_list.py
+++ python/rotated_list.py
@@ -11,3 +11,38 @@
 
 # Input: head = [0,1,2], k = 4
 # Output: [2,0,1]
+
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head:
+            return head
+
+        tail = head
+        count = 1
+
+        while tail.next:
+            tail = tail.next
+            count += 1
+            
+
+        k = k % count # to avoid redundant rotations
+
+        if k == 0:
+            return head
+        
+        # node before the rotating node
+        curr = head
+        for i in range(count - k - 1):
+            curr = curr.next
+
+        newHead = curr.next
+        curr.next = None
+        tail.next = head
+        return newHead   
