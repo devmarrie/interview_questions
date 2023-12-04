5ee4e41a740d6d1340f7764f14d6cbe89273b783
devmarrie <annemarrieakeyo42@gmail.com>
2023-12-04 10:03:09 +0300

    check the base cases eg if null then solve recur

--- python/same_tree.py
+++ python/same_tree.py
@@ -13,3 +13,22 @@
 
 # Input: p = [1,2], q = [1,null,2]
 # Output: false
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        leftNode = self.isSameTree(p.left, q.left)
+        rightNode = self.isSameTree(p.right, q.right)
+
+        if leftNode == True and rightNode == True:
+            return True
