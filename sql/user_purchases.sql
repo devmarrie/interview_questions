68dc83b25d16c1fffd2aa8835ed37bd8fe1b577f
devmarrie <annemarrieakeyo42@gmail.com>
2023-12-06 11:39:10 +0300

    the trick was finding the 7 days after the curr

--- sql/user_purchases.sql
+++ sql/user_purchases.sql
@@ -7,3 +7,13 @@
 -- 3	120	milk	2020-03-18	176
 -- 4	108	banana	2020-03-18	862
 -- 5	130	milk	2020-03-28	333
+
+with cte as(
+select user_id, created_at, 
+    lag(created_at) over(partition by user_id order by created_at) as prev
+from amazon_transactions 
+)
+select distinct u.user_id
+from cte u
+where u.created_at <= u.prev + interval '7' day
+    and u.prev is not null;
