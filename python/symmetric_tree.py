bb91ac540b7d0b55fc78ef73afaaefa2ccfac2c8
devmarrie <annemarrieakeyo42@gmail.com>
2023-12-07 08:34:29 +0300

    root same first node same so start from first node

--- python/symmetric_tree.py
+++ python/symmetric_tree.py
@@ -12,3 +12,22 @@
 # Input: root = [1,2,2,null,3,null,3]
 # Output: false
 
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+      if not root:
+            return True
+
+      def isMirror(left, right):
+          if not left and not right:
+              return True
+          if not left or not right:
+              return False
+          return (left.val == right.val) and isMirror(left.left, right.right) and isMirror(left.right, right.left)
+
+      return isMirror(root.left, root.right)
