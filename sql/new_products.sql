f5b15a3dd02e8e650802914fce7455590513344c
devmarrie <annemarrieakeyo42@gmail.com>
2023-12-08 20:33:54 +0300

    question

---
+++ sql/new_products.sql
@@ -0,0 +1,10 @@
+-- You are given a table of product launches by company by year. 
+-- Write a query to count the net difference between the number of products companies 
+-- launched in 2020 with the number of products companies launched in the previous year. 
+-- Output the name of the companies and a net difference of net products released for 2020 compared to the previous year.
+-- year	company_name	product_name
+-- 2019	Toyota	Avalon
+-- 2019	Toyota	Camry
+-- 2020	Toyota	Corolla
+-- 2019	Honda	Accord
+-- 2019	Honda	Passport
