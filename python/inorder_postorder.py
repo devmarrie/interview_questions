d18ccbceb23136e60ea55e23009d53aa6ac28fb9
devmarrie <annemarrieakeyo42@gmail.com>
2023-12-11 11:55:37 +0300

    start by building the right then the left for easy poping

--- python/inorder_postorder.py
+++ python/inorder_postorder.py
@@ -11,3 +11,27 @@
 
 # Input: inorder = [-1], postorder = [-1]
 # Output: [-1]
+from typing import Optional, List
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if not inorder or not postorder:
+            return None
+
+        root_val = postorder.pop()
+        root = TreeNode(root_val)
+        
+        # Find the index of the root value in inorder list
+        root_index = inorder.index(root_val)
+
+        # Build right and left subtrees using recursion
+        root.right = self.buildTree(inorder[root_index + 1:], postorder)
+        root.left = self.buildTree(inorder[:root_index], postorder)
+
+        return root
+
+
