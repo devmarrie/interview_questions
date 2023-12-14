756712ee6dd05896409037a2e89d7c80c7470238
devmarrie <annemarrieakeyo42@gmail.com>
2023-12-14 19:00:34 +0300

    use alinked list when you see pointers , create dummys at each node link them at the end

--- python/pop_next_pointers.py
+++ python/pop_next_pointers.py
@@ -23,3 +23,30 @@
 
 # Input: root = []
 # Output: []
+
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        curr = root
+
+        while curr:
+            """while on a level create a linked list"""
+            dummy = Node(0)
+            tail = dummy
+            while curr:
+                if curr.left:
+                    tail.next = curr.left
+                    tail = tail.next
+                
+                if curr.right:
+                    tail.next = curr.right
+                    tail = tail.next
+                curr = curr.next
+            curr = dummy.next #we have connected the children now link them to parent
+        return root
