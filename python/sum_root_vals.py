df0eb3b67527db92e4833766bc184f51d7282fe9
devmarrie <annemarrieakeyo42@gmail.com>
2023-12-20 09:30:49 +0300

    the trick is multiplying the current number by 10 before adding a node's val

--- python/sum_root_vals.py
+++ python/sum_root_vals.py
@@ -28,3 +28,23 @@
 # The root-to-leaf path 4->9->1 represents the number 491.
 # The root-to-leaf path 4->0 represents the number 40.
 # Therefore, sum = 495 + 491 + 40 = 1026.
+
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        
+        def dfs(node, num):
+            if not node:
+                return 0
+            num = (10 * num ) + node.val
+
+            if not node.left and not node.right:
+                return num
+            return dfs(node.left, num) + dfs(node.right, num)
+        return dfs(root,0)
+
