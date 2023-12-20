837ac0e18358a9bc5d3b4876ca023d36f3ee2a8f
devmarrie <annemarrieakeyo42@gmail.com>
2023-12-20 09:56:50 +0300

    the trick  is in finding each separately and summing them up by date

--- sql/premium_vs_freemium.sql
+++ sql/premium_vs_freemium.sql
@@ -8,3 +8,32 @@
 -- 2020-08-17	45	9
 -- 2020-08-18	10	7
 -- 2020-08-21	32	17
+
+where n.non_paying > y.paying
+
+with nos as (
+select d.date, sum(d.downloads) as non_paying
+from ms_user_dimension u
+inner join ms_acc_dimension a
+on u.acc_id = a.acc_id
+inner join ms_download_facts d
+on u.user_id = d.user_id
+where a.paying_customer = 'no'
+group by d.date
+),
+yeses as (
+select d.date, sum(d.downloads) as paying
+from ms_user_dimension u
+inner join ms_acc_dimension a
+on u.acc_id = a.acc_id
+inner join ms_download_facts d
+on u.user_id = d.user_id
+where a.paying_customer = 'yes'
+group by d.date
+)
+select n.date, n.non_paying, y.paying
+from nos n
+inner join yeses y
+on n.date = y.date
+where n.non_paying > y.paying
+order by n.date;
