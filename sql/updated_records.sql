4489b4ad3c7d2cb2796f52d4b22f6ed2a11ed5d6
devmarrie <annemarrieakeyo42@gmail.com>
2023-12-21 22:00:40 +0300

    the secret is in finding the max salalry

--- sql/updated_records.sql
+++ sql/updated_records.sql
@@ -2,3 +2,9 @@
 -- Find the current salary of each employee assuming that salaries increase each year. 
 -- Output their id, first name, last name, department ID, and current salary. 
 -- Order your list by employee ID in ascending order.
+
+select id, first_name, last_name, department_id,
+max(salary)
+from ms_employee_salary
+group by id
+order by id;
