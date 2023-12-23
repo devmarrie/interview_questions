364630d7d6a7fa89c8cfedf51a71b979c26c07da
devmarrie <annemarrieakeyo42@gmail.com>
2023-12-23 09:34:47 +0300

    left and right rep the sum of each of their sides sowe add them like that to root, max with the split and just continue dfsing

--- python/max_path_sum.py
+++ python/max_path_sum.py
@@ -17,3 +17,25 @@
 # Input: root = [-10,9,20,null,null,15,7]
 # Output: 42
 # Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
+
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = [root.val]
+        def dfs(root):
+            if not root:
+                return 0
+            leftMax = dfs(root.left)
+            rightMax = dfs(root.right)
+            leftMax = max(leftMax, 0)
+            rightMax = max(rightMax, 0)
+
+            res[0] = max(res[0], root.val + leftMax + rightMax)
+            return root.val + max(leftMax, rightMax)
+        dfs(root)
+        return res[0]
