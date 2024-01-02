58f492688cad2ab784b4004b7e5e08abbc85bc15
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-02 14:28:39 +0300

    this works pretty well and its what I would go for the difference comes in the return cause int is inaccessible

--- python/complete_tree_nodes.py
+++ python/complete_tree_nodes.py
@@ -20,3 +20,18 @@
 
 # Input: root = [1]
 # Output: 1
+
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+
+        def dfs(node):
+            if  not node:
+                return 0
+            return dfs(node.left) + dfs(node.right) + 1
+        return dfs(root)
