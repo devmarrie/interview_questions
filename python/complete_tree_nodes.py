1caff386ddae092d4f07e2dc5c865eec4946e6b2
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-02 15:03:19 +0300

    taking advantage of the binary structure

--- python/complete_tree_nodes.py
+++ python/complete_tree_nodes.py
@@ -27,11 +27,32 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+# class Solution:
+#     def countNodes(self, root: Optional[TreeNode]) -> int:
+
+#         def dfs(node):
+#             if  not node:
+#                 return 0
+#             return dfs(node.left) + dfs(node.right) + 1
+#         return dfs(root)
+
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
-
-        def dfs(node):
+        if not root: return 0
+        # calculate left and right heights
+        def lheight(node):
             if  not node:
                 return 0
-            return dfs(node.left) + dfs(node.right) + 1
-        return dfs(root)
+            return lheight(node.left) + 1
+        
+        def rheight(node):
+            if  not node:
+                return 0
+            return rheight(node.right) + 1
+        
+        #compare the heights and find tt if complete and balanced
+        l, r = lheight(root), rheight(root)
+        if l > r:
+            return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+        else:
+            return (2**l) - 1
