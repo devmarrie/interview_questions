743213bbd3b9003d293d70b7571c9c30be1aa4a4
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-04 09:27:07 +0300

    In this dfs approach we use adepth that ensures we do not repeat a node after passing through it

--- python/right_side_view.py
+++ python/right_side_view.py
@@ -16,3 +16,22 @@
 
 # Input: root = []
 # Output: []
+
+from typing import Optional, List
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        lst = []
+        def dfs(node, depth):
+            if not node:
+                return 
+            if depth == len(lst):
+                lst.append(node.val)
+            dfs(node.right, depth + 1)
+            dfs(node.left, depth + 1)
+        dfs(root, 0)
+        return lst
