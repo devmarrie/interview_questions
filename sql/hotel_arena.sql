c9cafe4c8211e53fe5335c8e98c783f0a96bff5e
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-05 09:14:26 +0300

    question

---
+++ sql/hotel_arena.sql
@@ -0,0 +1,12 @@
+-- Find the number of rows for each review score earned by 'Hotel Arena'. 
+-- Output the hotel name (which should be 'Hotel Arena'), 
+-- review score along with the corresponding number of rows with that score for the specified hotel.
+-- Table: hotel_reviews
+
+-- Expected Output
+-- hotel_name	reviewer_score	count(*)
+-- Hotel Arena	7.5	2
+-- Hotel Arena	9.6	2
+-- Hotel Arena	4.6	1
+-- Hotel Arena	3.8	1
+-- Hotel Arena	4.2	1
