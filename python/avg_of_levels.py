edb46ec6416bbab3d7f6965c0d7eeb12eb00a4dd
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-05 09:12:21 +0300

    irerate te queue while popping the first element and makiing it equal to ight side while also adding its children to the queue, using the level length of the queue

--- python/avg_of_levels.py
+++ python/avg_of_levels.py
@@ -13,3 +13,35 @@
 
 # Input: root = [3,9,20,15,7]
 # Output: [3.00000,14.50000,11.00000]
+
+from typing import Optional, List
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        lst = []
+
+        if not root:
+            return lst
+        queue = [root]
+
+        while queue:
+            level_sum = 0
+            level_count = len(queue)
+
+            for _ in range (level_count):
+                curr_node = queue.pop(0)
+                level_sum += curr_node.val
+
+                if curr_node.left:
+                    queue.append(curr_node.left)
+
+                if curr_node.right:
+                    queue.append(curr_node.right)
+
+            level_avg = level_sum / level_count
+            lst.append(level_avg)
+        return lst
