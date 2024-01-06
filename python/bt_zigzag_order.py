6f542d544b56b25b426cc5940bee4a5da2b82109
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-06 09:20:45 +0300

    if normal justappend the value if not ie even append at te begining.

--- python/bt_zigzag_order.py
+++ python/bt_zigzag_order.py
@@ -16,3 +16,37 @@
 
 # Input: root = []
 # Output: []
+
+from typing import Optional, List
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        lst = []
+        if not root:
+            return lst
+        queue = [root]
+        lr = True 
+
+        while queue:
+            level_len = len(queue)
+            level_lst = []
+
+            for _ in range (level_len):
+                curr_node = queue.pop(0)
+                if lr:
+                    level_lst.append(curr_node.val)
+                else:
+                    level_lst.insert(0, curr_node.val)
+
+                if curr_node.left:
+                    queue.append(curr_node.left)
+                if curr_node.right:
+                    queue.append(curr_node.right)
+
+            lst.append(level_lst)
+            lr = not lr
+        return lst
