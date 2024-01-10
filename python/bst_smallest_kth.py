4e15d6bbd4f8a18debb4aca5b4404307c90aea90
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-10 14:27:49 +0300

    the way a search tree works left side is always smaller than right

--- python/bst_smallest_kth.py
+++ python/bst_smallest_kth.py
@@ -23,3 +23,24 @@
  
 
 # Follow up: If the BST is modified often (i.e., we can do insert and delete operations) and you need to find the kth smallest frequently, how would you optimize?
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        lst = []
+        def dfs(node):
+            if not node:
+                return []
+            dfs(node.left)
+            lst.append(node.val)
+            dfs(node.right)
+        dfs(root)
+
+        for i in range(len(lst)):
+            if i +1 == k:
+                return lst[i]
+        
