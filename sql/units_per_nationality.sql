383712aa19a02d56ef4d19a256dd325e9a9979e2
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-10 14:31:16 +0300

    find aptment unit per country then count them

--- sql/units_per_nationality.sql
+++ sql/units_per_nationality.sql
@@ -4,3 +4,17 @@
 
 -- Sort records by the apartments count in descending order.
 -- Tables: airbnb_hosts, airbnb_units
+
+with cte as (
+select distinct h.nationality,
+    u.unit_id
+from airbnb_hosts h
+left join airbnb_units u
+on h.host_id = u.host_id
+where h.age < 30 and u.unit_type = 'Apartment'
+)
+select nationality,
+    count(unit_id) as apartment_count
+from cte
+group by nationality
+order by count(unit_id) desc
