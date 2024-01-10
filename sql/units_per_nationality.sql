bbb265dd3a4cb764d9cb74acfec776af42a407f2
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-10 14:30:14 +0300

    question

---
+++ sql/units_per_nationality.sql
@@ -0,0 +1,6 @@
+-- Find the number of apartments per nationality that are owned by people under 30 years old.
+
+-- Output the nationality along with the number of apartments.
+
+-- Sort records by the apartments count in descending order.
+-- Tables: airbnb_hosts, airbnb_units
