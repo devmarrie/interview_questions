62e4d7451420c505b4129eff9d6cd8d2b1e6998d
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-11 11:36:25 +0300

    just the normateratel way we iterate a bst but in the for loop chek if the prev is > than curr

--- python/validate_bst.py
+++ python/validate_bst.py
@@ -20,3 +20,28 @@
 # Input: root = [5,1,4,null,null,3,6]
 # Output: false
 # Explanation: The root node's value is 5 but its right child's value is 4.
+
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        lst = []
+
+        def dfs(node):
+            if not node:
+                return []
+            dfs(node.left)
+            lst.append(node.val)
+            dfs(node.right)
+
+        dfs(root)
+
+        for i in range(1, len(lst)):
+            if lst[i] <= lst[i-1]:
+                return False
+        return True     
