0767d5d4bca5ae81682e7f1af35a0ba74055ddf4
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-11 11:33:09 +0300

    question

---
+++ sql/apple_product_count.sql
@@ -0,0 +1,13 @@
+-- Find the number of Apple product users and the number of total users with a device and group the counts by language. 
+-- Assume Apple products are only MacBook-Pro, iPhone 5s, and iPad-air. 
+-- Output the language along with the total number of Apple users and users with any device. 
+-- Order your results based on the number of total users in descending order.
+-- Tables: playbook_events, playbook_users.
+
+-- Expected Output
+-- language	n_apple_user	n_total_users
+-- english	    11	                45
+-- spanish	    3	                9
+-- japanese	2	                6
+-- french	    0	                5
+-- russian	    0	                5
