a2880d39ad712ec392aa883c069ca3494987e0bc
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-15 10:46:21 +0300

    iterate through the matrix to find an island once found run a dfs through it

--- python/number_of_islands.py
+++ python/number_of_islands.py
@@ -23,3 +23,37 @@
 #   ["0","0","0","1","1"]
 # ]
 # Output: 3
+import collections
+from typing import List
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = set() #to ensure a cell is not visited twice
+        islands = 0
+
+        def bfs(r,c):
+            q = collections.deque()
+            visited.add((r,c))
+            q.append((r,c))
+
+            while q:
+                row, col = q.popleft()
+                directions = [[1,0],[-1,0],[0,1],[0,-1]] #T,B,R,L
+                for dr, dc in directions:
+                    r,c = row + dr, col + dc
+                    if (r in range(rows) and
+                       c in range(cols) and 
+                       grid[r][c] == "1" and
+                       (r,c) not in visited):
+                       q.append((r,c))
+                       visited.add((r,c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    bfs(r,c) # iterate it horizontally and vertically
+                    islands += 1
+        return islands
