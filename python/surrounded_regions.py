ae748affa32725a4e95297a4e4903462f8787b9b
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-17 10:56:32 +0300

    the easiest way is to convert the border regions and the rest just to x

--- python/surrounded_regions.py
+++ python/surrounded_regions.py
@@ -18,3 +18,41 @@
 
 # Input: board = [["X"]]
 # Output: [["X"]]
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        rows, cols =len(board), len(board[0])
+        
+        # Check the ones at the edge and mark them wwith a different xcter
+        def dfs(i,j):
+            if (i < 0 or i == rows or
+               j < 0 or j == cols
+               or board[i][j] != "O"):
+               return
+            board[i][j] = "T"
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+        for i in range(rows):
+            for j in range(cols):
+                if (board[i][j] == "O" and 
+                (i in [0, rows -1] or j in [0, cols -1])):
+                   dfs(i,j)
+
+        # Check the ones surrounded and x them
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+
+        # Remark the ones at the edge wth '0's
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == "T":
+                    board[i][j] = "O"
+
+        
