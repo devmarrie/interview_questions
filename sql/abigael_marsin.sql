5f7821d16040cba66d4f9413055af3ca18f662d9
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-17 10:53:50 +0300

    question

---
+++ sql/abigael_marsin.sql
@@ -0,0 +1,8 @@
+-- Count the number of movies that Abigail Breslin was nominated for an oscar.
+-- Table: oscar_nominees
+-- year:int
+-- category:varchar
+-- nominee:varchar
+-- movie:varchar
+-- winner:bool
+-- id:int
