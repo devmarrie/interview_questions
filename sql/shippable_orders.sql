8fec177708f2991139202ce818c4eeca945072f7
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-18 09:40:34 +0300

    question

---
+++ sql/shippable_orders.sql
@@ -0,0 +1,17 @@
+-- Find the percentage of shipable orders.
+
+-- Consider an order is shipable if the customer's address is known.
+-- Tables: orders, 
+-- id:int
+-- cust_id:int
+-- order_date:date
+-- timeorder_details:varchar
+-- total_order_cost:int
+
+-- customers
+-- id:int
+-- first_name:varchar
+-- last_name:varchar
+-- city:varchar
+-- address:varchar
+-- phone_number:varchar
