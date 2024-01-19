6941588e80b2e521eb07f9979564aa8fc06c6ec7
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-19 07:50:19 +0300

    ensure all the right columns are picked

--- sql/heart_posts.sql
+++ sql/heart_posts.sql
@@ -14,3 +14,8 @@
 -- post_keywords:varchar
 -- post_date:datetime
 
+select distinct p.post_id, p.poster, p.post_text, p.post_keywords, p.post_date
+from facebook_reactions r
+right join facebook_posts p
+on r. post_id = p.post_id
+where r.reaction = 'heart';
