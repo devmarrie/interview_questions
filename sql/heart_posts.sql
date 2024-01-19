29d06bf4d47952973ed254c94e03bd62a5b7e5cc
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-19 07:49:44 +0300

    question

---
+++ sql/heart_posts.sql
@@ -0,0 +1,16 @@
+-- Find all posts which were reacted to with a heart. For such posts output all columns from facebook_posts table.
+-- Tables: facebook_reactions, facebook_posts
+
+-- poster:int
+-- friend:int
+-- reaction:varchar
+-- date_day:int
+-- post_id:int
+
+-- facebook_posts
+-- post_id:int
+-- poster:int
+-- post_text:varchar
+-- post_keywords:varchar
+-- post_date:datetime
+
