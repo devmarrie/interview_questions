eb70d935c58c9856d886efa006f061dd87ea2001
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-22 10:32:43 +0300

    question

---
+++ sql/host_and_guest.sql
@@ -0,0 +1,13 @@
+-- Find matching hosts and guests pairs in a way that they are both of the same gender and nationality.
+-- Output the host id and the guest id of matched pair.
+-- Tables: airbnb_hosts
+-- host_id:int
+-- nationality:varchar
+-- gender:varchar
+-- age:int
+
+-- airbnb_guests
+-- guest_id:int
+-- nationality:varchar
+-- gender:varchar
+-- age:int
