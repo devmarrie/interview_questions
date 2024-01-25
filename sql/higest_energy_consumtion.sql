8465977c3484a8f5913a97b19dbeae2eae4c6c93
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-25 12:49:37 +0300

    question

---
+++ sql/higest_energy_consumtion.sql
@@ -0,0 +1,3 @@
+-- Find the date with the highest total energy consumption from the Meta/Facebook data centers. 
+-- Output the date along with the total energy consumption across all data centers.
+-- Tables: fb_eu_energy, fb_asia_energy, fb_na_energy
