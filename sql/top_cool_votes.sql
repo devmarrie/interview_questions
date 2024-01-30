288db35edd777be5f72aa32fbbffa89e84af48a3
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-30 21:50:03 +0300

    question

---
+++ sql/top_cool_votes.sql
@@ -0,0 +1,8 @@
+-- Find the review_text that received the highest number of  'cool' votes.
+
+-- Output the business name along with the review text with the highest numbef of 'cool' votes.
+-- Table: yelp_reviews
+-- Expected 
+-- business_name	review_text
+-- Roka Akor	    I hate to admit it, but it had been a long while since my last visit to Roka Akor. I deserve a hand slap. But last week, I had the perfect excuse to p
+-- Lunardis	    This is the nicest grocery store in the city. I actually met my wife at this grocery store while shopping for avocados.
