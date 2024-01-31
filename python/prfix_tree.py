0edda8200cf6a67b8c13e892f4c8595c6ee7c20b
devmarrie <annemarrieakeyo42@gmail.com>
2024-01-31 08:57:23 +0300

    implemment a TrieNode class whichcreates the node and append each letter with its children nodes

--- python/prfix_tree.py
+++ python/prfix_tree.py
@@ -25,3 +25,45 @@
 # trie.startsWith("app"); // return True
 # trie.insert("app");
 # trie.search("app");     // return True
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.final = False
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for w in word:
+            if w not in node.children:
+                node.children[w] = TrieNode()
+            node = node.children[w]
+        node.final = True # we have inserted all and reached the final one 
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for w in word:
+            if w not in node.children:
+                return False
+            node = node.children[w]
+        return node.final
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self .root
+        for w in prefix:
+            if w not in node.children:
+                return False
+            node = node.children[w]
+        return True
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
