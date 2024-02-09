2cc36cd6f00ba7734e4f1490bd46d63c21e835ab
devmarrie <annemarrieakeyo42@gmail.com>
2024-02-09 08:14:46 +0300

    now using substring_index and trim

--- sql/reveiw_of_categories.sql
+++ sql/reveiw_of_categories.sql
@@ -7,3 +7,26 @@
 -- Restaurants	1703
 -- Event Planning & Services	162
 -- Food Delivery Services	30
+SELECT
+  TRIM(SUBSTRING_INDEX(SUBSTRING_INDEX(categories, ';', n), ';', -1)) AS category,
+  SUM(review_count) AS review_cnt
+FROM
+  yelp_business
+JOIN (
+  SELECT 1 AS n UNION ALL
+  SELECT 2 UNION ALL
+  SELECT 3 UNION ALL
+  SELECT 4 UNION ALL
+  SELECT 5 UNION ALL
+  SELECT 6 UNION ALL
+  SELECT 7 UNION ALL
+  SELECT 8 UNION ALL
+  SELECT 9 UNION ALL
+  SELECT 10
+) AS numbers
+ON
+  CHAR_LENGTH(categories) - CHAR_LENGTH(REPLACE(categories, ';', '')) >= n - 1
+GROUP BY
+  category
+ORDER BY
+  review_cnt DESC;
