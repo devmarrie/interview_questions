0b3c9cd9fd7438f756f1511ec82a7e5715e84c19
devmarrie <annemarrieakeyo42@gmail.com>
2024-02-13 17:57:51 +0300

    First create the Prefic node then then add to it and define a recursive dfs that backtracks

--- python/word_search_II.py
+++ python/word_search_II.py
@@ -13,3 +13,50 @@
 
 # Input: board = [["a","b"],["c","d"]], words = ["abcb"]
 # Output: []
+
+from typing import List
+class TrieNode:
+    def __init__(self):
+        self.children = {} #child: nextnode
+        self.end = False
+    
+    def addWord(self, word):
+        # Create our perfix tree
+        curr = self
+        for w in word:
+            if w not in curr.children:
+                curr.children[w] = TrieNode()
+            curr = curr.children[w]
+        curr.end = True
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+
+        for wrd in words:
+            root.addWord(wrd)
+
+        row, col = len(board), len(board[0])
+        res, visited = set(), set()
+
+        def dfs(r, c, node, word):
+            if (r < 0 or c < 0 or r == row or c == col
+               or board[r][c] not in node.children or (r,c) in visited):
+               return
+            visited.add((r,c))
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            if node.end:
+                res.add(word)
+            
+            dfs(r+1, c, node, word)
+            dfs(r-1, c, node, word)
+            dfs(r, c+1, node, word)
+            dfs(r, c-1, node, word)
+            visited.remove((r,c))
+
+        for i in range(row):
+            for j in range(col):
+                dfs(i, j, root, "")
+        
+        return list(res)
