52b4566506b4636ad19f032c7b4a25fc9b5dfddc
devmarrie <annemarrieakeyo42@gmail.com>
2024-02-14 09:30:58 +0300

    realised that limit wount cut it cause of the ranking

---
+++ sql/states_with_5_star_bs.sql
@@ -0,0 +1,13 @@
+-- Find the top 5 states with the most 5 star businesses. 
+-- Output the state name along with the number of 5-star businesses and 
+-- order records by the number of 5-star businesses in descending order. 
+-- In case there are ties in the number of businesses, return all the unique states. 
+-- If two states have the same result, sort them in alphabetical order.
+-- Table: yelp_business
+-- Expected Output:
+-- state	five_star_counts
+-- AZ	10
+-- ON	5
+-- NV	4
+-- IL	3
+-- OH	3
