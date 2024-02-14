f626ef486ac4067035cb37317459742be9553629
devmarrie <annemarrieakeyo42@gmail.com>
2024-02-14 09:34:48 +0300

    create a hash map to map digits to its xcters, then backtrack on the digit for each xcter

--- python/letter_combi_phone_no.py
+++ python/letter_combi_phone_no.py
@@ -26,3 +26,30 @@
 #     0 <= digits.length <= 4
 #     digits[i] is a digit in the range ['2', '9'].
 
+from typing import List
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        lst = []
+        numToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        def backtrack(i, currString):
+            if len(currString) == len(digits):
+                lst.append(currString)
+                return
+            
+            for c in numToChar[digits[i]]:
+                backtrack(i+1, currString + c)
+        
+        if digits:
+            backtrack(0, "")
+        return lst
+
