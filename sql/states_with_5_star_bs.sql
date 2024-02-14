8e54459cfb9e87d5a12b2cc0746fdf273434860c
devmarrie <annemarrieakeyo42@gmail.com>
2024-02-14 09:31:42 +0300

    when ranking using two orderby clauses messes it up

--- sql/states_with_5_star_bs.sql
+++ sql/states_with_5_star_bs.sql
@@ -11,3 +11,20 @@
 -- NV	4
 -- IL	3
 -- OH	3
+
+with cte as(
+select state, count(stars) as stars 
+from yelp_business
+where stars = 5
+group by state
+order by stars desc
+), 
+r as (
+select state, stars,
+  dense_rank() over (order by stars desc) as rnk
+from cte
+)
+select state, stars as five_star_counts
+from r
+where rnk <= 4
+order by stars desc, state asc
