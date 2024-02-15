7801461f11fc7e56cd16fa1527325b7c60f8f008
devmarrie <annemarrieakeyo42@gmail.com>
2024-02-15 15:08:20 +0300

    we use regexp

---
+++ sql/winneries.sql
@@ -0,0 +1,14 @@
+-- Find all wineries which produce wines by possessing aromas of plum, cherry, rose, or hazelnut. To make it more simple, look only for singular form of the mentioned aromas. HINT: if one of the specified words is just a substring of another word, this should not be a hit, but a miss.
+
+-- Example Description: Hot, tannic and simple, with cherry jam and currant flavors accompanied by high, tart acidity and chile-pepper alcohol heat.
+
+-- Therefore the winery Bella Piazza is expected in the results.
+-- Table: winemag_p1
+-- Expected output
+
+-- winery
+-- Boudreaux Cellars
+-- Goldeneye
+-- Pine Ridge
+-- Hopler
+-- Bella Piazza
