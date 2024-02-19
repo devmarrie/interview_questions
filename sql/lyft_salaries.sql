dd8b4a50cfedb1c270aa1e33d09375629e3cbdec
devmarrie <annemarrieakeyo42@gmail.com>
2024-02-19 08:12:21 +0300

    the where and or clauses

--- sql/lyft_salaries.sql
+++ sql/lyft_salaries.sql
@@ -1,2 +1,5 @@
 -- Find all Lyft drivers who earn either equal to or less than 30k USD or equal to or more than 70k USD.
 -- Output all details related to retrieved records.
+
+select * from lyft_drivers
+where yearly_salary <= 30000 or yearly_salary >= 70000;
