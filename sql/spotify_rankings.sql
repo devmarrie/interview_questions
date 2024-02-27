b0d7a08833a056f233056a78c13458c5cabc2fd7
devmarrie <annemarrieakeyo42@gmail.com>
2024-02-27 07:59:10 +0300

    just a genius one way to think abour it

--- sql/spotify_rankings.sql
+++ sql/spotify_rankings.sql
@@ -2,3 +2,8 @@
 -- Output the artist name along with the corresponding number of occurrences.
 -- Order records by the number of occurrences in descending order.
 -- Table: spotify_worldwide_daily_song_ranking
+
+select artist, count(*) as n_occurences
+from spotify_worldwide_daily_song_ranking
+group by artist
+order by 2 DESC;
