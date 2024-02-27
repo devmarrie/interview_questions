c8a325039b81b6b22f6d5465a8a2fb88ddc6e167
devmarrie <annemarrieakeyo42@gmail.com>
2024-02-27 07:58:34 +0300

    they want the count for each artist

---
+++ sql/spotify_rankings.sql
@@ -0,0 +1,4 @@
+-- Find how many times each artist appeared on the Spotify ranking list
+-- Output the artist name along with the corresponding number of occurrences.
+-- Order records by the number of occurrences in descending order.
+-- Table: spotify_worldwide_daily_song_ranking
