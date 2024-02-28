ae256eaebc7265b4d5d03bc9e5c3705993ac0887
devmarrie <annemarrieakeyo42@gmail.com>
2024-02-28 09:11:50 +0300

    the secret is in knowing we have to start with a ( and the open ( must be higher than the ) for us to add a)

--- python/generate_parantesis.py
+++ python/generate_parantesis.py
@@ -11,3 +11,28 @@
 
 # Input: n = 1
 # Output: ["()"]
+
+from typing import List
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        res = []
+
+        def backtrack(open, closed):
+            if open == closed == n:
+                res.append("".join(stack))
+                return
+
+            if open < n:
+                stack.append("(")
+                backtrack(open + 1, closed)
+                stack.pop()
+            
+            if closed < open:
+                stack.append(")")
+                backtrack(open, closed + 1)
+                stack.pop()
+
+        backtrack(0,0)
+        return res
+        
