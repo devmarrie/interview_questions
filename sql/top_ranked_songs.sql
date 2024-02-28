0dd9885b823e037a673c5cf2041c84f1a38cd128
devmarrie <annemarrieakeyo42@gmail.com>
2024-02-28 09:08:14 +0300

    the songs are already ranked

---
+++ sql/top_ranked_songs.sql
@@ -0,0 +1,3 @@
+-- Find songs that have ranked in the top position. Output the track name and the number of times it ranked at the top. 
+-- Sort your records by the number of times the song was in the top position in descending order.
+-- Table: spotify_worldwide_daily_song_ranking
