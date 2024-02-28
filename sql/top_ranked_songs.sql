6c089e781480e0af6ca20a0b0891bde53c4c13ef
devmarrie <annemarrieakeyo42@gmail.com>
2024-02-28 09:09:05 +0300

    again count(*) comes in handy

--- sql/top_ranked_songs.sql
+++ sql/top_ranked_songs.sql
@@ -1,3 +1,9 @@
 -- Find songs that have ranked in the top position. Output the track name and the number of times it ranked at the top. 
 -- Sort your records by the number of times the song was in the top position in descending order.
 -- Table: spotify_worldwide_daily_song_ranking
+
+select trackname, count(*) as times_top1
+from spotify_worldwide_daily_song_ranking
+where position = 1
+group by trackname
+order by 2 desc;
