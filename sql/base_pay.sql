2edde737bb712607794b6aa676d6cfe7c4148903
devmarrie <annemarrieakeyo42@gmail.com>
2024-02-29 09:59:15 +0300

    the where clause

--- sql/base_pay.sql
+++ sql/base_pay.sql
@@ -1,3 +1,7 @@
 -- Find the base pay for Police Captains.
 -- Output the employee name along with the corresponding base pay.
 -- Table: sf_public_salaries
+
+select employeename, basepay 
+from sf_public_salaries
+where jobtitle = 'CAPTAIN III (POLICE DEPARTMENT)';
