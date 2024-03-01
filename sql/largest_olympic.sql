c7337395fed1beae968787de5e4bb1cd0cb321dc
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-01 11:20:11 +0300

    the secret is in counting the distinct names only

--- sql/largest_olympic.sql
+++ sql/largest_olympic.sql
@@ -2,3 +2,9 @@
 -- The Olympics game is a combination of the year and the season, and is found in the 'games' column. 
 -- Output the Olympics along with the corresponding number of athletes.
 -- Table: olympics_athletes_events
+
+select games, count(distinct name) as  athletes_count
+from olympics_athletes_events
+group by games
+order by 2 desc
+limit 1;
