18ad070738170a66dcf61ba8520b75cf173a6c99
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-01 11:19:01 +0300

    question

---
+++ sql/largest_olympic.sql
@@ -0,0 +1,4 @@
+-- Find the Olympics with the highest number of athletes. 
+-- The Olympics game is a combination of the year and the season, and is found in the 'games' column. 
+-- Output the Olympics along with the corresponding number of athletes.
+-- Table: olympics_athletes_events
