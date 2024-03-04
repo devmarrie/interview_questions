66dd7a84cfd1afa4856971930e2c2ca918e66237
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-04 11:01:39 +0300

    it is a list and we are given the treenode def

--- python/sorted_to _bst.py
+++ python/sorted_to _bst.py
@@ -15,3 +15,21 @@
 # Input: nums = [1,3]
 # Output: [3,1]
 # Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+
+from typing import List, Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        def helper(l, r):
+            if l > r: #cause in a balanced tree its always supposed to be less
+               return None
+            m = (l + r) // 2
+            root = TreeNode(nums[m])
+            root.left = helper(l, m - 1)
+            root.right  = helper(m + 1, r)
+            return root
+        return helper(0, len(nums) - 1)
