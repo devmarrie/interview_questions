e5114e319f9d4bb2a5162d02c28e2804d2d0fa63
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-04 11:12:50 +0300

    remeber to handle the duplicates

--- sql/libraries.sql
+++ sql/libraries.sql
@@ -2,3 +2,7 @@
 --  but their notice preference definition is set to email.
 -- Output the library code.
 -- Table: library_usage
+
+select distinct home_library_code 
+from library_usage
+where circulation_active_year = 2016 and notice_preference_definition = 'email' and provided_email_address = 'FALSE';
