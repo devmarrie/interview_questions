9e5fde420f36d5bf630b85826e696991bd7afc53
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-04 11:11:51 +0300

    question

---
+++ sql/libraries.sql
@@ -0,0 +1,4 @@
+-- Find libraries who haven't provided the email address in circulation year 2016
+--  but their notice preference definition is set to email.
+-- Output the library code.
+-- Table: library_usage
