2a97fd58e1cc3f3d24c1462bc831a7074db369d3
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-06 22:57:24 +0300

    comaring a column by an agregate of another , use over

--- sql/avg_salaries.sql
+++ sql/avg_salaries.sql
@@ -1,3 +1,7 @@
 -- Compare each employee's salary with the average salary of the corresponding department.
 -- Output the department, first name, and salary of employees along with the average salary of that department.
 -- Table: employee
+
+select department, first_name, salary,
+avg(salary) over(partition by department)
+from employee;
