87d31afb1eeeaa155e882ca9ddc93ddb0c267631
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-06 22:56:42 +0300

    question

---
+++ sql/avg_salaries.sql
@@ -0,0 +1,3 @@
+-- Compare each employee's salary with the average salary of the corresponding department.
+-- Output the department, first name, and salary of employees along with the average salary of that department.
+-- Table: employee
