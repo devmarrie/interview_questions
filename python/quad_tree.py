0d7d290841ef7622bd5accac54e1930b4fcd91dd
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-06 22:52:24 +0300

    the trick is in the basecase if same  and not same

--- python/quad_tree.py
+++ python/quad_tree.py
@@ -50,3 +50,36 @@
 # The topRight have different values so we divide it into 4 sub-grids where each has the same value.
 # Explanation is shown in the photo below:
 
+from typing import List
+# Definition for a QuadTree node.
+class Node:
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+
+
+class Solution:
+    def construct(self, grid: List[List[int]]) -> 'Node':
+
+        def dfs(n, r, c):
+            allSame = True
+            for i in range(n):
+                for j in range(n):
+                    if grid[r][c] != grid[r + i][c +j]:
+                        allSame = False
+                        break
+            if allSame:
+                return Node(grid[r][c], True) # others will be none
+            
+            n = n // 2
+            topLeft = dfs(n, r, c)
+            topRight = dfs(n, r, c + n)
+            bottomLeft = dfs(n, r + n, c)
+            bottomRight = dfs(n, r + n, c + n)
+            return Node(0, False, topLeft, topRight, bottomLeft, bottomRight)
+        return dfs(len(grid), 0, 0)
+
