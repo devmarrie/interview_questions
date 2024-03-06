a744ad018d195d151bd4e3da0dbe4f0e78b32368
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-06 22:50:04 +0300

    divide the list use slow and fast t find mid then merge

--- python/reshuffle_linkedlst.py
+++ python/reshuffle_linkedlst.py
@@ -16,3 +16,51 @@
 
 # Input: head = []
 # Output: []
+
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #Base case
+        if not head or not head.next:
+            return head
+        
+        #Divide the nodes recursively
+        left = head
+        right = self.getMid(head)
+        tmp = right.next
+        right.next = None
+        right = tmp
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+        return self.merge(left, right)
+
+    # get the middle node
+    def getMid(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+        
+    #Merge the sorted nodes
+    def merge(self, left, right):
+        tail = dummy = ListNode()
+        while left and right:
+            if left.val < right.val:
+                tail.next = left
+                left = left.next
+            else:
+                tail.next = right
+                right = right.next
+            tail = tail.next
+        if left:
+            tail.next = left
+        if right:
+            tail.next = right
+        return dummy.next
