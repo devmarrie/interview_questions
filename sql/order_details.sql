2d270c2e7acb9635d612d3908e9f188ffa18c8e4
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-07 10:42:05 +0300

    just the where clause

--- sql/order_details.sql
+++ sql/order_details.sql
@@ -3,3 +3,10 @@
 -- Output the order date, details and cost along with the first name.
 -- Order records based on the customer id in ascending order.
 -- Tables: customers, orders
+
+select c.first_name, o.order_date , o.order_details, o.total_order_cost
+from customers c
+inner join orders o
+on c.id = o.cust_id
+where c.first_name = 'Jill' or c.first_name = 'Eva'
+order by c.id;
