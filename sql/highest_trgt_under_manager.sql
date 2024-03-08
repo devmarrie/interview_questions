8c5b07c072de70d82ac5e02fb23e3304e57fa2fe
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-08 11:50:38 +0300

    rank them then pick number 1

--- sql/highest_trgt_under_manager.sql
+++ sql/highest_trgt_under_manager.sql
@@ -2,3 +2,14 @@
 -- Output the first name of the employee and target achieved. 
 -- The solution should show the highest target achieved under manager_id=13 and which employee(s) achieved it.
 -- Table: salesforce_employees
+
+with cte as (
+select first_name, target, 
+dense_rank()over(order by target desc) as rnk
+from salesforce_employees
+where manager_id = 13
+)
+
+select first_name, target
+from cte
+where rnk = 1
