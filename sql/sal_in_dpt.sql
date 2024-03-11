6cfbabb33710c918adbca76ec49761260f48db11
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-11 10:45:42 +0300

    first rank by dpt then pick the first

--- sql/sal_in_dpt.sql
+++ sql/sal_in_dpt.sql
@@ -1,3 +1,14 @@
 -- Find the employee with the highest salary per department.
 -- Output the department name, employee's first name along with the corresponding salary.
 -- Table: employee
+
+with cte as (
+select first_name, department, salary, 
+dense_rank()over(partition by department order by salary desc) as rnk
+from employee
+order by department, salary desc
+)
+select department, first_name as employee_name, salary
+from cte
+where rnk = 1
+order by salary desc
