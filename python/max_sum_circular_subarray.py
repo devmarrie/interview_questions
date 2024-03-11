c1ae6886c9bce3eac1c6f4ef4cfeaa8619e33ff7
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-11 10:48:17 +0300

    use the total and min to help verify the max

--- python/max_sum_circular_subarray.py
+++ python/max_sum_circular_subarray.py
@@ -24,4 +24,18 @@
 # Output: -2
 # Explanation: Subarray [-2] has maximum sum -2.
 
+from typing import List
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        gloMax, gloMin = nums[0], nums[0]
+        curMax, curMin, total = 0, 0, 0
+
+        for n in nums:
+            curMax = max(curMax + n, n)
+            curMin = min(curMin + n, n)
+            total += n
+            gloMax = max(gloMax, curMax)
+            gloMin = min(gloMin, curMin)
+        return max(gloMax, total - gloMin) if gloMax > 0 else gloMax
+
  
