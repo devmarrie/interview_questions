d779fe7a5b3144279d12b6a6af814dc98bcd37be
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-12 10:33:01 +0300

    the trick is in finding the manager salaries first then the greatest employee salo

--- sql/employees_and_mng_salo.sql
+++ sql/employees_and_mng_salo.sql
@@ -1,3 +1,15 @@
 -- Find employees who are earning more than their managers. 
 -- Output the employee's first name along with the corresponding salary.
 -- Table: employee
+
+with cte as (
+select  distinct e.id, e.first_name as mng, e.salary as mng_sal
+from employee e
+inner join employee m
+on e.id = m.manager_id
+)
+select e.first_name, e.salary
+from employee e
+inner join cte m
+on e.manager_id = m.id
+where e.salary > m.mng_sal
