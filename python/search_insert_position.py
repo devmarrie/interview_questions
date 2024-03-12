09298021c37086f109099df0a20521d15fccccc8
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-12 10:30:08 +0300

    the trick is in using a while loop to iterate less than or equal to right

--- python/search_insert_position.py
+++ python/search_insert_position.py
@@ -18,3 +18,20 @@
 
 # Input: nums = [1,3,5,6], target = 7
 # Output: 4
+
+from typing import List
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        #o(logn) entails skipping some values
+        l, r = 0, len(nums)-1 
+
+        while l <= r:
+            m = (l+r) // 2
+
+            if target == nums[m]:
+                return m
+            if target > nums[m]:
+                l = m + 1
+            else:
+                r = m - 1
+        return l
