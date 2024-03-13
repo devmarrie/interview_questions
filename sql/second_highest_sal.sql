8f6efe1edf13dd57ae0ae94afbde45a1b2ddb1d8
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-13 12:07:56 +0300

    rank then find the second

--- sql/second_highest_sal.sql
+++ sql/second_highest_sal.sql
@@ -1,2 +1,12 @@
 -- Find the second highest salary of employees.
 -- Table: employee
+
+with cte as (
+select distinct first_name, salary,
+    dense_rank()over(order by salary desc) as rnk
+from employee
+order by salary desc
+)
+select salary
+from cte
+where rnk = 2
