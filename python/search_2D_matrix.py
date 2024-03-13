dc04ad3a6e0310c7c0af6f4005370d3a22b70ad7
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-13 12:10:00 +0300

    in BS we use the median to skip values

--- python/search_2D_matrix.py
+++ python/search_2D_matrix.py
@@ -15,3 +15,35 @@
 
 # Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
 # Output: false
+
+from typing import List
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row, col = len(matrix), len(matrix[0])
+        if not matrix or not matrix[0]:
+            return False
+        
+        #Find the row containing the target
+        top, bot = 0, row - 1
+        while top <= bot:
+            mid_row = (top + bot) // 2
+            if target > matrix[mid_row][-1]:
+                top = mid_row + 1
+            elif target < matrix[mid_row][0]:
+                bot = mid_row - 1
+            else:
+                break
+        if not (top <= bot):
+            return False
+        fin = (top + bot) // 2
+        #now locate the target
+        l, r = 0, col - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if target > matrix[fin][mid]:
+                l = mid + 1
+            elif target < matrix[fin][mid]:
+                r = mid - 1
+            else:
+                return True
+        return False
