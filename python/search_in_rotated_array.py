9f9931eb775a8e48975e2a33007fdc583ef46acc
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-19 16:45:59 +0300

    first check if the first value is less than the middle then it means left is the max side, check if T >m or < first move left forward

--- python/search_in_rotated_array.py
+++ python/search_in_rotated_array.py
@@ -22,3 +22,26 @@
 
 # Input: nums = [1], target = 0
 # Output: -1
+
+from typing import List
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l , r = 0, len(nums)-1
+        
+        while l <= r:
+            m = (l+r) // 2
+            if target == nums[m]:
+                return m
+            
+            if nums[l] <= nums[m]:
+                if target > nums[m] or target < nums[l]:
+                    l = m + 1
+                else:
+                    r = m-1
+            # right side
+            else:
+                if target < nums[m] or target > nums[r]:
+                    r = m - 1
+                else:
+                    l = m + 1
+        return -1
