278ffb9c53929c6b2fde11579e462014f9f30645
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-19 16:47:47 +0300

    each customer regardless so left join

--- sql/customer_details.sql
+++ sql/customer_details.sql
@@ -1,3 +1,9 @@
 -- Find the details of each customer regardless of whether the customer made an order. Output the customer's first name, last name, and the city along with the order details.
 -- You may have duplicate rows in your results due to a customer ordering several of the same items. Sort records based on the customer's first name and the order details in ascending order.
 -- Tables: customers, orders
+
+select c.first_name, c.last_name, c.city, d.order_details
+from customers c
+left join orders d
+on c.id = d.cust_id 
+order by c.first_name, d.order_details;
