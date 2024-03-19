4655f6879d2fd35943adc3af4def35d84b90c666
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-19 16:47:11 +0300

    question

---
+++ sql/customer_details.sql
@@ -0,0 +1,3 @@
+-- Find the details of each customer regardless of whether the customer made an order. Output the customer's first name, last name, and the city along with the order details.
+-- You may have duplicate rows in your results due to a customer ordering several of the same items. Sort records based on the customer's first name and the order details in ascending order.
+-- Tables: customers, orders
