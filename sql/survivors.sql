f57f9dff5cc7b6351e1782d884a3976825f61af4
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-20 13:53:38 +0300

    question

---
+++ sql/survivors.sql
@@ -0,0 +1,12 @@
+-- Make a report showing the number of survivors and non-survivors by passenger class.
+
+-- Classes are categorized based on the pclass value as:
+
+-- pclass = 1: first_class
+
+-- pclass = 2: second_classs
+
+-- pclass = 3: third_class
+
+-- Output the number of survivors and non-survivors by each class.
+-- Table: titani
