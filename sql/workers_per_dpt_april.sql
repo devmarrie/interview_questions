768a5e072d5e29854598546fe457ea83899c8391
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-21 08:25:11 +0300

    qustion

---
+++ sql/workers_per_dpt_april.sql
@@ -0,0 +1,6 @@
+-- Find the number of workers by department who joined in or after April.
+
+-- Output the department name along with the corresponding number of workers.
+
+-- Sort records based on the number of workers in descending order.
+-- Table: worker
