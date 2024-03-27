0e92b31636deafaaf02150b2fe38699f599dd884
devmarrie <annemarrieakeyo42@gmail.com>
2024-03-27 10:08:03 +0300

    count all given the where clause no groupby needed

--- sql/april_admin_dpt_employees.sql
+++ sql/april_admin_dpt_employees.sql
@@ -1,2 +1,5 @@
 -- Find the number of employees working in the Admin department that joined in April or later.
 -- Table: worker
+
+select count(*) as n_admins from worker
+where month(joining_date) >= 4 and department = 'Admin';
