2ec4d80c0f7bd9eec38ac4ace145a9015fb32524
devmarrie <annemarrieakeyo42@gmail.com>
2024-04-15 22:24:31 +0300

    if left is grater move right closer by a minus

--- python/peak_element.py
+++ python/peak_element.py
@@ -19,3 +19,18 @@
 # Input: nums = [1,2,1,3,5,6,4]
 # Output: 5
 # Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+
+from typing import List
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = l + ((r-l)//2)
+            # left greater
+            if m > 0 and nums[m] < nums[m-1]:
+                r = m - 1
+            elif m < len(nums) -1 and nums[m] < nums[m+1]:
+                l = m + 1
+            else:
+                return m
